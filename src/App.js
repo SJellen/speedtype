4d73748c43795c398aadfa36b9f3caa70dd84371
scott jellen <scottjellen@scotts-MBP.fios-router.home>
2020-05-18 15:13:18 -0400

    text area

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+    
+    const [words, setWords] = useState('')
+
+    
+    function handleChange(e)  {
+      e.preventDefault()
+      const {value} = e.target
+      setWords(value)
+    }
+       
+    
+    console.log(words)
+
   return (
     <div className="App">
-     <h1>Speed Typer</h1>
-     <textarea />
-     <h4>Time remaining</h4>
+     <h1>Speed Typer 88</h1>
+     <textarea 
+     onChange={handleChange}
+     value={words}
+     />
+     <h4>Time remaining: </h4>
      <button>Start</button>
      <h1>Word Count</h1>
+  
     </div>
   );
 }
